d1ef64e508a5db340c8ff0f94b6da1570719fe11
FirozuiShaker <furouzi.abolfazl@yahoo.com>
2023-09-29 13:08:25 +0200

    complete the project

---
+++ script.js
@@ -0,0 +1,9 @@
+var names=["yakoov", "James" ,"taghi","ali" , "jorjina","ajid","mehdi"];
+
+for(var i=0 ; i<names.length ; i++){
+    var first= names[i].charAt(0);
+    if(first=="j" || first=="J" ){
+            console.log("Goodbye "+names[i])
+    }else{ console.log("Hello "+names[i])
+    }
+}
