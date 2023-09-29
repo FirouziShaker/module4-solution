bafc37931bb54cb3482370e72c3b11a84aa1ba1e
FirozuiShaker <furouzi.abolfazl@yahoo.com>
2023-09-29 17:15:00 +0200

    add files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var names=["yakoov", "James" ,"taghi","ali" , "jorjina","ajid","mehdi"];
+var names=["yakoov", "James" ,"john","taghi","ali" , "jorjina","ajid","mehdi"];
 
 for(var i=0 ; i<names.length ; i++){
     var first= names[i].charAt(0);
